26dd35da4e3000d618d4df967a0021eb90333718
Emiliano Borzelli <vibrains@gmail.com>
2018-11-12 11:12:31 -0500

    added contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,11 +4,26 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
 const contact = () => (
-  <Layout>
-  <h1>Hi from the contact page</h1>
-  <p>Welcome to contact page</p>
-  <Link to="/">Go back to the homepage</Link>
-  </Layout>
-  )
+	<Layout>
+	<form name="contact" method="POST" netlify>
+	<p class="hidden">
+	<label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+	</p>
+	<p>
+	<label>Name: <input type="text" name="name" /></label>   
+	</p>
+	<p>
+	<label>Email: <input type="email" name="email" /></label>
+	</p>
+	<p>
+	<label>Message: <textarea name="message"></textarea></label>
+	</p>
+	<p>
+	<button type="submit">Send</button>
+	</p>
+	</form>
+	<Link to="/">Go back to the homepage</Link>
+	</Layout>
+	)
 
 export default contact
