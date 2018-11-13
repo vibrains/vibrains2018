96f0c63c45bd2c36e1265487656171e02f1f93f5
Emiliano <vibrains@gmail.com>
2018-11-13 18:13:04 -0500

    updates to contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,20 +5,10 @@ import Layout from '../components/layout'
 const contact = () => (
 	<Layout>
 
-  <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-  <input type="hidden" name="contact" value="contact" />  
-  <div>
-  <input placeholder="Full Name" name="name" type="text" required />
-  </div>
-  <div>
-  <input placeholder="Email Address" name="email" type="email" required />
-  </div>
-  <div>
-  <textarea placeholder="Message" name="message"></textarea>
-  </div>
-  <p>
-  <button type="submit">Send</button>
-  </p>
+  <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+  <input type="text" name="name" />
+  <input type="email" name="email" />
+  <textarea name="message"></textarea>
   </form>
 
   <Link to="/">Go back to the homepage</Link>
