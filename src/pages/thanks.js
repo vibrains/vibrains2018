41ae7aa83061ef814c2821d8b482bab8929211dd
Emiliano <vibrains@gmail.com>
2018-12-31 10:34:24 -0500

    thank you page styling

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { Link } from 'gatsby';
+import { FiCornerDownLeft } from 'react-icons/fi';
 
 export default () => (
   <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-                <h1>Thank you!</h1>
-                <p>I will reach back as soon as possible</p>
-            </div>
-          </div>
-          </section>
+  <section className="section">
+  <div className="container">
+  <div className="content">
+  <h1 className="text-white">Thank you!</h1>
+  <p className="text-white">I will reach back as soon as possible.</p>
+  <p>
+  <FiCornerDownLeft/><Link to="/">Home</Link>  
+  </p>
+  </div>
+  </div>
+  </section>
   </Layout>
-);
+  );
